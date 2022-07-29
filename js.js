7506c0968979f9bfa404ff6837bbe29bc116bcc5
Gabriel Martinez <gabi.martineez97@gmail.com>
2022-07-28 21:09:10 -0300

    primer commit

---
+++ js.js
@@ -0,0 +1,5 @@
+for (let i = 1; i <= 10; i++){
+    let nombreCliente = prompt ("Ingrece su nombre");
+    let tratamiento = prompt ("Lavadao, Detallado o Lavado y Detallado")
+    alert("Turno N* "+i+" "+"Gracias"+" "+nombreCliente+" "+ "por confiar en nosotros!")
+}
